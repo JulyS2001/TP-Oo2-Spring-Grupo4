0aaa312be76b55d83f646d513600ca7638988b46
JulyS <rukysorli@gmail.com>
2025-06-02 20:34:55 -0300

    Prueba creacion de clase

---
+++ src/main/java/com/oo2/grupo4/entities/Persona.java
@@ -0,0 +1,20 @@
+package com.oo2.grupo4.entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+public class Persona {
+
+}
