3303615eadf4d5c8cc107c91ffbd4d6c4898a310
Axel De Pietro <axel.dp44@gmail.com>
2025-06-02 21:41:13 -0300

    add: cliente atributos y relacion

--- src/main/java/com/oo2/grupo4/entities/Cliente.java
+++ src/main/java/com/oo2/grupo4/entities/Cliente.java
@@ -1,5 +1,21 @@
 package com.oo2.grupo4.entities;
 
-public class Cliente {
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @AllArgsConstructor @ToString
+public class Cliente extends Persona{
+	
+	private String nroCliente;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idTicket")
+	private List<Ticket> lstTicket;
 }
