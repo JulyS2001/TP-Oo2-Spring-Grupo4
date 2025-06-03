a21b7c8d294ff8b391a1b43b17721d490b4b8562
JulyS <rukysorli@gmail.com>
2025-06-02 22:11:11 -0300

    Add: Clase Entity Actualizacion

--- src/main/java/com/oo2/grupo4/entities/Actualizacion.java
+++ src/main/java/com/oo2/grupo4/entities/Actualizacion.java
@@ -1,5 +1,42 @@
 package com.oo2.grupo4.entities;
 
+import java.time.LocalDate;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity @Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString 
 public class Actualizacion {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idActualizacion")
+	private int id; 
+	
+	private String contenido; 
+	private LocalDate fechaActualizacion; 
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idEmpleado")
+	private Empleado empleado; 
+	
+	@ManyToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idTicket")
+	private Ticket ticket;
+	
+	
+	
 
 }
