9325f6221880a618b1665206bc98dc4afe4a5dcd
JulyS <rukysorli@gmail.com>
2025-06-02 22:23:25 -0300

    Add: Clase Entity Estado

--- src/main/java/com/oo2/grupo4/entities/Estado.java
+++ src/main/java/com/oo2/grupo4/entities/Estado.java
@@ -1,5 +1,34 @@
 package com.oo2.grupo4.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
 public class Estado {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idEstado")
+	private int idEstado;
+	
+	private String tipo; 
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idTicket")
+	private List<Ticket> tickets;
 
 }
