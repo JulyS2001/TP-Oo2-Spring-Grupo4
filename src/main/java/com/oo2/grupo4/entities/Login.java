713b60349c052ae46f47a4e3affcd3df564b58d0
Axel De Pietro <axel.dp44@gmail.com>
2025-06-02 21:59:18 -0300

    add: login, atributos y relacion

--- src/main/java/com/oo2/grupo4/entities/Login.java
+++ src/main/java/com/oo2/grupo4/entities/Login.java
@@ -1,5 +1,31 @@
 package com.oo2.grupo4.entities;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Setter @Getter @AllArgsConstructor @ToString
 public class Login {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idLogin")
+	private int idLogin;
+	private String correo;
+	private String contraseña;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idPersona")
+	private Persona persona;
+	
 }
