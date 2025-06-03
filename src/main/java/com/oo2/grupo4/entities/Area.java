94d483014c3aa2bd2cf3d153ccd1d51d00e95eda
JulyS <rukysorli@gmail.com>
2025-06-02 22:16:32 -0300

    Add: Clase Entity Area

--- src/main/java/com/oo2/grupo4/entities/Area.java
+++ src/main/java/com/oo2/grupo4/entities/Area.java
@@ -1,5 +1,34 @@
 package com.oo2.grupo4.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
 public class Area {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idArea")
+	private int id; 
+	
+	private String nombre; 
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idEmpleado")
+	private List<Empleado> empleados; 
 
 }
