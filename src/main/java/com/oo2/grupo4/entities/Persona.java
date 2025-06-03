0a1c061634df9f960476cda875aead845a6c2496
Axel De Pietro <axel.dp44@gmail.com>
2025-06-02 21:08:02 -0300

    add: persona entity

--- src/main/java/com/oo2/grupo4/entities/Persona.java
+++ src/main/java/com/oo2/grupo4/entities/Persona.java
@@ -3,6 +3,7 @@ package com.oo2.grupo4.entities;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -11,10 +12,21 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+@Getter @Setter @AllArgsConstructor @ToString
 public class Persona {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idPersona")
+	private int id;
+	private String nombre;
+	private String apellido;
+	private long dni;	
+	
 }
