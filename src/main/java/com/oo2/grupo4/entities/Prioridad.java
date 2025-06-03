c771c24dc5f28eff358eb7abdb5e94f9a8a78b2d
JulyS <rukysorli@gmail.com>
2025-06-02 22:18:57 -0300

    Add: clase entity Prioridad

--- src/main/java/com/oo2/grupo4/entities/Prioridad.java
+++ src/main/java/com/oo2/grupo4/entities/Prioridad.java
@@ -1,5 +1,34 @@
 package com.oo2.grupo4.entities;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter @Setter @AllArgsConstructor @NoArgsConstructor @ToString
 public class Prioridad {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "idPrioridad")
+	private int id; 
+	
+	private String tipo; 
+	
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "idTicket")
+	private List<Ticket> tickets;
 
 }
