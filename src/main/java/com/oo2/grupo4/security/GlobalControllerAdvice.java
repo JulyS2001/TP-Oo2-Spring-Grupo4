5cc31793626467ac5e3f8daa6ba86dee01f71053
JulyS <rukysorli@gmail.com>
2025-06-16 23:44:26 -0300

    Add: GlobalControllerAdvice para traer usuario y rol de la sesion a las vistas

---
+++ src/main/java/com/oo2/grupo4/security/GlobalControllerAdvice.java
@@ -0,0 +1,33 @@
+package com.oo2.grupo4.security;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.oo2.grupo4.security.UserDetailsImpl;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+    @ModelAttribute("usuario")
+    public String usuario(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
+            return userDetails.getLogin().getPersona().getNombre();
+        }
+        return null;
+    }
+
+    @ModelAttribute("rol")
+    public String rol(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        if (userDetails != null) {
+            return userDetails.getAuthorities().stream()
+                    .findFirst()
+                    .map(auth -> auth.getAuthority()) // Ej: ROLE_ADMIN
+                    .orElse("ROLE_CLIENTE");
+        }
+        return null;
+    }
+
+}
